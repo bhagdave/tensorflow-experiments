582ea6633c42973d625d4e6eadbbb3b0685f285d
Dave Gill <david.g.h.gill@gmail.com>
2023-11-24 16:01:54 +0000

    Added code for using multiple images per job

--- repair-replace-category.py
+++ repair-replace-category.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import os
-from keras.models import Sequential
+from keras.models import Model, Sequential
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, RandomFlip, RandomRotation
+from keras.layers import Input, concatenante, Dense, Conv2D, MaxPooling2D, Flatten, Dropout, RandomFlip, RandomRotation
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras import backend as K
 
@@ -10,6 +10,50 @@ image_folder = '/home/dave/Projects/tensorflow/tensorflow-experiments/images'
 image_height = 300
 image_width = 300
 model_name = 'repair-replace-cross'
+
+class TwoImageGenerator(tf.keras.utils.Sequence):
+    def __init__(self, directory, batch_size=32):
+        self.directory = directory
+        self.batch_size = batch_size
+        self.image_files = os.listdir(directory)
+        self.indexes = np.arrange(len(self.image_files) // 2)
+
+    def __len__(self):
+        return (len(self.indexes)
+
+    def load_and_preprocess_image(path):
+        image = tf.io.read_file(path)
+        image = tf.image.decode_jpeg(image, channels=3)
+        image = tf.image.resize(image, [image_height, image_width])
+        image /= 255.0
+        return image
+
+    def create_image_pairs(folder_path):
+        files = os.listdir(folder_path)
+        paired_files = {}
+
+        for file in files:
+            job_id = file.split('_', 1)
+            if job_id not in paired_files:
+                paired_files[job_id] = []
+            paired_files[job_id].append(os.path.join(folder_path, file))
+
+        return [tuple(paired_files[job_id]) for job_id in paired_files if len(paired_files[job_id]) == 2]
+
+    def __getitem__(self, index):
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        batch_1  = []
+        batch_2  = []
+        labels = []
+        for idx in indexes:
+            img_name_1 = self.image_files[2*idx]
+            img_name_2 = self.image_files[2*idx+1]
+            # load and preprocess images
+            batch_1.append(img_1)
+            batch_2.append(img_2)
+            labels.append(label)
+        return [np.array(batch_1), np.array(batch_2)], np.array(labels)
+
 
 def f1_score(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
