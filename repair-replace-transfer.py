cd5a5ab0d74df8dd721a436a53c20fc666507035
Dave Gill <d.gill@stem.org.uk>
2023-11-28 22:25:01 +0000

    Using both images - added callbacks and using all cases in training and validation data

--- repair-replace-transfer.py
+++ repair-replace-transfer.py
@@ -11,12 +11,39 @@ from tensorflow.keras.optimizers import RMSprop
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from PIL import Image
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 image_folder = '/home/dave/Projects/tensorflow/tensorflow-experiments/images-new'
 model_name = 'repair-replace-transfer'
 image_height = 300
 image_width = 300
+
+early_stopping = EarlyStopping(
+    monitor='val_loss',  # You can use 'val_accuracy' or other metrics as well
+    patience=2,           # Number of epochs with no improvement after which training will stop
+    restore_best_weights=True  # Restores the model weights from the epoch with the best monitored value
+)
+
+class CustomEarlyStopping(tf.keras.callbacks.Callback):
+    def __init__(self, condition, verbose=0):
+        super(CustomEarlyStopping, self).__init__()
+        self.condition = condition
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs=None):
+        if self.condition(logs):
+            self.model.stop_training = True
+            if self.verbose > 0:
+                print(f"Custom early stopping triggered at epoch {epoch + 1}.")
+
+# Define your custom condition function
+def custom_condition(logs):
+    # You can define your condition based on loss, accuracy, or any other metric
+    return logs.get('loss') < 0.2  # Example: Stop if loss is less than 0.2
+
+# Create the custom callback
+custom_early_stopping = CustomEarlyStopping(condition=custom_condition, verbose=1)
 
 # Define a custom data generator
 class CustomImageDataGenerator:
@@ -29,7 +56,6 @@ class CustomImageDataGenerator:
         self.image_files = self.collect_image_files()
 
     def collect_image_files(self):
-        print("Debug: Collecting image files")
         image_files = {}
 
         for category in os.listdir(self.directory):
@@ -42,33 +68,53 @@ class CustomImageDataGenerator:
 
         return image_files
 
-    def generate_data(self):
+    def generate_data(self, is_training=True):
+        categories = os.listdir(self.directory)  # List of category folder names
+        all_cases = []  # Collect all cases in all categories
+
+        for category in categories:
+            category_path = os.path.join(self.directory, category)
+            image_files = os.listdir(category_path)
+            guids = set()
+
+            for image_file in image_files:
+                if image_file.endswith('.jpg'):
+                    guid, image_type = image_file.split('_')[:2]
+                    guids.add(guid)
+
+            all_cases.extend([(category, guid) for guid in guids])
+
         while True:
-            print ('DEBUG: Generating data')
             batch_images = []
             batch_labels = []
 
-            for _ in range(self.batch_size):
-                # Randomly select a category
-                selected_category = np.random.choice(list(self.image_files.keys()))
-                image_file = np.random.choice(self.image_files[selected_category])
-                
-                # Load and preprocess the selected image
-                image = Image.open(image_file)
-                image = image.resize((self.image_width, self.image_height))
-                image = np.array(image) / 255.0  # Normalize the pixel values
+            for (category, guid) in all_cases:
+                for image_type in ['close_up', 'damage_area']:
+                    image_file = f"{guid}_{image_type}.jpg"
+                    image_path = os.path.join(self.directory, category, image_file)
+                    image = Image.open(image_path)
+                    image = image.resize((self.image_width, self.image_height))
+                    image = np.array(image) / 255.0  # Normalize the pixel values
 
-                batch_images.append(image)
-                batch_labels.append(selected_category)  # Use category as label
+                    batch_images.append(image)
+                    batch_labels.append(category) 
 
-            batch_images = np.array(batch_images)
-            if self.class_mode == 'categorical':
-                # Convert category labels to one-hot encoding
-                unique_categories = list(self.image_files.keys())
-                batch_labels = [unique_categories.index(label) for label in batch_labels]
-                batch_labels = tf.keras.utils.to_categorical(batch_labels, len(unique_categories))
+                    if len(batch_images) == self.batch_size:
+                        batch_images = np.array(batch_images)
+                        if self.class_mode == 'categorical':
+                            # Convert image types to one-hot encoded labels
+                            batch_labels = np.array(batch_labels)
+                            unique_labels = np.unique(batch_labels)
+                            label_to_index = {label: i for i, label in enumerate(unique_labels)}
+                            batch_labels = [label_to_index[label] for label in batch_labels]
+                            batch_labels = tf.keras.utils.to_categorical(batch_labels, len(unique_labels))
 
-            yield batch_images, batch_labels
+                        yield batch_images, batch_labels
+
+                        # Clear the batch lists
+                        batch_images = []
+                        batch_labels = []
+
 
 # Use the custom data generator
 print("Debug: Train Directory:", os.path.join(image_folder, 'train/'))
@@ -114,7 +160,7 @@ model = Model(pre_trained_model.input, x)
 model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0.0001), metrics=['accuracy'])
 
 
-model.fit_generator(train_generator, epochs=40, validation_data=test_generator)
+model.fit_generator(train_generator, epochs=40, validation_data=test_generator, callbacks=[early_stopping, custom_early_stopping])
 
 model.save(f"{model_name}.h5")
 
