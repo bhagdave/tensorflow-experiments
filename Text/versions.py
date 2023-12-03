414e55b4795be2c420a2ee408251ca8002d9b2f3
Dave Gill <david.g.h.gill@gmail.com>
2023-12-03 14:26:25 +0000

    Added keras to versions

--- Text/versions.py
+++ Text/versions.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
+import keras
 import transformers
 
 print("TensorFlow version:", tf.__version__)
+print("Keras version:", keras.__version__)
 print("Transformers version:", transformers.__version__)
 
