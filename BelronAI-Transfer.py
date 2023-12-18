772063f04dee4d798c41d5c767904891885be613
Dave Gill <d.gill@stem.org.uk>
2023-12-18 10:09:41 +0000

    Round winner changes

--- BelronAI-Transfer.py
+++ BelronAI-Transfer.py
@@ -25,7 +25,14 @@ image_width = 300
 model_name = 'repair-replace-cross'
 batch_size = 8
 num_classes = 4
-learning_rate = 0.0001
+learning_rate = 0.0002
+dropout_rate1 = 0.15
+dropout_rate2 = 0.1
+regularisation_rate = 0.02
+early_stopping_patience = 12
+num_epochs = 40
+dense_layer_size = 1024
+
 
 class CustomImageDataGenerator:
     def __init__(self, directory, image_width, image_height, batch_size=batch_size, class_mode='categorical'):
@@ -132,12 +139,12 @@ x = Conv2D(3, (1, 1))(input_tensor)  # 1x1 convolution
 x = base_model(x)
 
 # Add a Dropout layer after VGG16 model
-x = Dropout(0.15)(x)  # 50% dropout
+x = Dropout(dropout_rate1)(x)  # 50% dropout
 
 # Add a new top layer with L2 regularisation
 x = Flatten()(x)
-x = Dense(1024, activation='relu', kernel_regularizer=regularizers.l2(0.02))(x)
-x = Dropout(0.1)(x)  # 50% dropout after the first Dense layer
+x = Dense(dense_layer_size, activation='relu', kernel_regularizer=regularizers.l2(regularisation_rate))(x)
+x = Dropout(dropout_rate2)(x)  # 50% dropout after the first Dense layer
 predictions = Dense(num_classes, activation='softmax')(x)
 
 # This is the model we will train
@@ -155,12 +162,12 @@ model.compile(optimizer=rmsprop_optimizer, loss='categorical_crossentropy', metr
 checkpoint = ModelCheckpoint('model-{epoch:03d}.keras', monitor='val_loss', save_best_only=True, mode='auto')
 # Define the early stopping criteria
 #early_stopping_loss = EarlyStopping(monitor='val_loss', min_delta=0.001,verbose=1, patience=4, mode='min')
-early_stopping_accuracy = EarlyStopping(monitor='val_accuracy', min_delta=0.001,verbose=1, patience=9, mode='max')
-early_stopping_f1 = EarlyStopping(monitor='val_f1_score',min_delta=0.001,verbose=1, patience=9, mode='max')
+early_stopping_accuracy = EarlyStopping(monitor='val_accuracy', min_delta=0.001,verbose=1, patience=early_stopping_patience, mode='max')
+early_stopping_f1 = EarlyStopping(monitor='val_f1_score',min_delta=0.001,verbose=1, patience=early_stopping_patience, mode='max')
 
 model.fit(
     x=train_generator.generate_data(),
-    epochs=40,
+    epochs=num_epochs,
     steps_per_epoch=train_generator.calculate_num_samples() // train_generator.batch_size,
     validation_data=validation_generator.generate_data(),
     validation_steps=validation_generator.calculate_num_samples() // validation_generator.batch_size,
