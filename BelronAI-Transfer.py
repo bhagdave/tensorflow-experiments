aabb71ccd62ec843b73218e9204697d707eda546
Dave Gill <david.g.h.gill@gmail.com>
2024-02-11 13:24:57 +0000

    Changed to Adam optimizer

--- BelronAI-Transfer.py
+++ BelronAI-Transfer.py
@@ -28,13 +28,13 @@ image_width = 224
 model_name = 'repair-replace-cross'
 batch_size = 8
 num_classes = 2
-learning_rate = 0.001
-dropout_rate1 = 0.3
+learning_rate = 0.0005
+dropout_rate1 = 0.4
 dropout_rate2 = 0.5
-regularisation_rate = 0.0002
-early_stopping_patience = 6
-num_epochs = 30
-dense_layer_size = 1024
+regularisation_rate = 0.0003
+early_stopping_patience = 10
+num_epochs = 100
+dense_layer_size = 1536
 
 
 # Initialize the CustomImageDataGenerator for training and validation
@@ -65,7 +65,7 @@ predictions = Dense(num_classes, activation='softmax')(x)  # Final layer with so
 
 model = Model(inputs=input_tensor, outputs=predictions)
 
-rmsprop_optimizer = RMSprop(learning_rate=learning_rate)
+rmsprop_optimizer = Adam(learning_rate=learning_rate)
 
 def scheduler(epoch, lr):
     if epoch < 20:
