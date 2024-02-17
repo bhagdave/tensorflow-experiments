067dc08c47e75996a2710ac3f97ef43c1d0917e5
Dave Gill <d.gill@stem.org.uk>
2024-02-17 08:15:48 +0000

    Using shared code and calculated epoch size

--- Belron-SimpleV2.py
+++ Belron-SimpleV2.py
@@ -3,6 +3,7 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 import random
 import json
+from SharedClasses import f1_score, CustomEarlyStopping, CustomImageDataGenerator
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
@@ -17,9 +18,9 @@ import datetime
 from tensorflow.keras.callbacks import TensorBoard
 os.environ['TF_GRPC_TIMEOUT'] = '3600'  # Set it to 1 hour (3600 seconds)
 
-image_folder = './images-new'
-image_height = 300
-image_width = 300
+image_folder = './images-new/close_up'
+image_height = 256
+image_width = 256
 model_name = 'belron-simplev2'
 batch_size = 8
 num_classes = 2
@@ -30,7 +31,7 @@ conv_3_units = 128
 dropout_rate = 0.5
 dense_1_units = 256 
 early_stopping = 13
-steps_per_epoch = 100
+steps_per_epoch = 462
 learning_rate = 0.001
 validation_steps = 100
 l2_regularization = 0.001
@@ -49,111 +50,6 @@ def scheduler(epoch, lr):
     else:
         return learning_rate * 0.001
 
-class CustomImageDataGenerator:
-    def __init__(self, directory, image_width, image_height, batch_size=batch_size, class_mode='categorical'):
-        self.directory = directory
-        self.image_width = image_width
-        self.image_height = image_height
-        self.batch_size = batch_size
-        self.class_mode = class_mode
-        self.image_files = self.collect_image_files()
-
-    def collect_image_files(self):
-        image_files = {}
-
-        for category in os.listdir(self.directory):
-            category_dir = os.path.join(self.directory, category)
-            if os.path.isdir(category_dir):
-                image_files[category] = []
-                for image_file in os.listdir(category_dir):
-                    if image_file.endswith('.jpg'):
-                        image_files[category].append(os.path.join(category_dir, image_file))
-
-        return image_files
-
-    def calculate_num_samples(self):
-        return sum(len(files) for files in self.image_files.values())
-
-    def generate_data(self, is_training=True):
-        categories = os.listdir(self.directory)  # List of category folder names
-        all_cases = []  # Collect all cases in all categories
-        if is_training:  # Only augment training data
-            datagen = ImageDataGenerator(
-                rotation_range=20,
-                width_shift_range=0.2,
-                height_shift_range=0.2,
-                zoom_range=0.2,
-                horizontal_flip=True,
-                fill_mode='nearest'
-            )
-
-        for category in categories:
-            category_path = os.path.join(self.directory, category)
-            image_files = os.listdir(category_path)
-            guids = set()
-
-            for image_file in image_files:
-                if image_file.endswith('.jpg'):
-                    guid, image_type = image_file.split('_')[:2]
-                    guids.add(guid)
-
-            all_cases.extend([(category, guid) for guid in guids])
-
-        while True:
-            random.shuffle(all_cases)  # Shuffle cases for better training performance
-            batch_images = []
-            batch_labels = []  # Initialize an empty list for batch labels
-
-            for (category, guid) in all_cases:
-                combined_image= None
-                for image_type in ['close_up', 'damage_area']:
-                    image_file = f"{guid}_{image_type}.jpg"
-                    image_path = os.path.join(self.directory, category, image_file)
-                    image = Image.open(image_path)
-                    image = image.resize((self.image_width, self.image_height))
-                    image_array = np.array(image) / 255.0
-
-                    if image_type == 'close_up' and is_training:
-                        image_array = datagen.random_transform(image_array)  # Apply transformations
-                    
-                    if combined_image is None:
-                        combined_image = image_array
-                    else:
-                        combined_image = np.concatenate((combined_image, image_array), axis=-1)  # Combine along the channel axis
-
-                batch_labels.append(category)
-                batch_images.append(combined_image)
-
-                if len(batch_images) == self.batch_size:
-                    batch_images = np.array(batch_images)
-                    if self.class_mode == 'categorical':
-                        # Convert labels to numerical format
-                        label_to_index = {label: i for i, label in enumerate(categories)}
-                        batch_labels = [label_to_index[label] for label in batch_labels]
-                        batch_labels = tf.keras.utils.to_categorical(batch_labels, len(categories))
-
-                    yield batch_images, batch_labels
-
-                    # Clear the batch lists
-                    batch_images = []
-                    batch_labels = []  # Clear the batch labels
-            
-            # If there are not enough images left to form a full batch, discard them
-            batch_images = []
-            batch_labels = []
-
-
-class CustomEarlyStopping(tf.keras.callbacks.Callback):
-    def __init__(self, condition, verbose=0):
-        super(CustomEarlyStopping, self).__init__()
-        self.condition = condition
-        self.verbose = verbose
-
-    def on_epoch_end(self, epoch, logs=None):
-        if self.condition(logs):
-            self.model.stop_training = True
-            if self.verbose > 0:
-                print(f"Custom early stopping triggered at epoch {epoch + 1}.")
 
 # Define your custom condition function
 def custom_condition(logs):
@@ -163,24 +59,14 @@ def custom_condition(logs):
 # Create the custom callback
 custom_early_stopping = CustomEarlyStopping(condition=custom_condition, verbose=1)
 
-def f1_score(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    recall = true_positives / (possible_positives + K.epsilon())
-    f1_val = 2 * (precision * recall) / (precision + recall + K.epsilon())
-    return f1_val
-
 # Initialize the CustomImageDataGenerator for training and validation
 train_generator = CustomImageDataGenerator(os.path.join(image_folder, 'train/'), image_width, image_height, batch_size=batch_size)
 validation_generator = CustomImageDataGenerator(os.path.join(image_folder, 'validate/'), image_width, image_height, batch_size=batch_size)
 
 def model_builder():
-    print("Building model")
     model = Sequential()
 
-    model.add(SeparableConv2D(conv_1_units, (3, 3), activation='relu', input_shape=(image_height, image_width, 6)))
+    model.add(SeparableConv2D(conv_1_units, (3, 3), activation='relu', input_shape=(image_height, image_width, 3)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(2, 2))
 
@@ -213,9 +99,7 @@ checkpoint = ModelCheckpoint('model-{epoch:03d}.keras', monitor='val_accuracy', 
 early_stopping = EarlyStopping(monitor='val_loss', patience=early_stopping, verbose=1, mode='auto', restore_best_weights=True)
 learning_rate_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
-# model.fit_generator(train_generator, epochs=num_epochs, validation_data=validation_generator, callbacks=[early_stopping, learning_rate_callback])
 
-print("Training model")
 # Fit the model
 model.fit(
     train_generator.generate_data(),
