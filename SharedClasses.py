7f6f93ec78817917fe75373fd264091601dffdea
Dave Gill <d.gill@stem.org.uk>
2024-02-21 18:20:33 +0000

    Amended IMage transofmration params in the training

--- SharedClasses.py
+++ SharedClasses.py
@@ -38,10 +38,10 @@ class CustomImageDataGenerator:
         all_cases = []  # Collect all cases in all categories
         if is_training:  # Only augment training data
             datagen = ImageDataGenerator(
-                rotation_range=20,
-                width_shift_range=0.2,
-                height_shift_range=0.2,
-                zoom_range=0.2,
+                rotation_range=30,
+                width_shift_range=0.3,
+                height_shift_range=0.3,
+                zoom_range=0.3,
                 horizontal_flip=True,
                 fill_mode='nearest'
             )
