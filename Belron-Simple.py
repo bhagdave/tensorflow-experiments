7b2cc99ddb9b310663f215c30eb597d6af17736b
Dave Gill <david.g.h.gill@gmail.com>
2024-02-25 08:46:58 +0000

    Hyperparameter tuning for simple

--- Belron-Simple.py
+++ Belron-Simple.py
@@ -17,30 +17,30 @@ from PIL import Image
 image_folder = './images-new/close_up'
 image_height = 256
 image_width = 256
-model_name = 'belron-simplei-256'
+model_name = 'belron-simple-256'
 batch_size = 8
 num_classes = 2
 num_epochs = 100 
-conv_1_units = 160
-dropout_rate = 0.5
-dense_1_units = 160 
-dense_2_units = 192 
-dense_3_units = 192
-dense_4_units = 96
+conv_1_units = 256
+dropout_rate = 0.2
+dense_1_units = 32 
+dense_2_units = 512 
+dense_3_units = 768
+dense_4_units = 320
 early_stopping = 10
-steps_per_epoch = 462
-learning_rate = 0.0001
-validation_steps = 31
+steps_per_epoch = 404
+learning_rate = 0.00001
+validation_steps = 30
 
 def scheduler(epoch, lr):
-    if epoch < 20:
+    if epoch < 10:
         return learning_rate
-    elif epoch < 35:
-        return learning_rate * .5
-    elif epoch < 50:
+    elif epoch < 20:
         return learning_rate * .1
+    elif epoch < 40:
+        return learning_rate * .01
     else:
-        return learning_rate * .05
+        return learning_rate * .001
 
 
 # Define your custom condition function
@@ -56,7 +56,6 @@ train_generator = CustomImageDataGenerator(os.path.join(image_folder, 'train/'),
 validation_generator = CustomImageDataGenerator(os.path.join(image_folder, 'validate/'), image_width, image_height, batch_size=batch_size)
 
 def model_builder():
-    print("Building model")
         # ... other layers ...
     model = Sequential()
     model.add(Conv2D(conv_1_units, (3,3), activation='relu', input_shape=(image_height, image_width, 3)))
@@ -95,7 +94,7 @@ model.compile(
 
 checkpoint = ModelCheckpoint('model-{epoch:03d}.keras', monitor='val_accuracy', save_best_only=True, mode='auto')
 # Define the early stopping criteria
-early_stopping = EarlyStopping(monitor='val_accuracy', patience=early_stopping, verbose=1, mode='auto', restore_best_weights=True)
+early_stopp = EarlyStopping(monitor='val_loss', patience=early_stopping, verbose=1, mode='min', restore_best_weights=True)
 learning_rate_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
 # model.fit_generator(train_generator, epochs=num_epochs, validation_data=validation_generator, callbacks=[early_stopping, learning_rate_callback])
@@ -108,7 +107,7 @@ model.fit(
     validation_data=validation_generator.generate_data(),
     validation_steps=validation_steps,
     verbose=1,
-    callbacks=[learning_rate_callback, checkpoint, custom_early_stopping, early_stopping],
+    callbacks=[learning_rate_callback, checkpoint, custom_early_stopping, early_stopp],
 )
 
 # Save the model
