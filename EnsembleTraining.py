9bce9cbbacd6d9d8b704e5296c94dcbc04e2b758
Dave Gill <d.gill@stem.org.uk>
2024-03-01 21:31:36 +0000

    Fixes to ensemble training

--- EnsembleTraining.py
+++ EnsembleTraining.py
@@ -1,57 +1,60 @@
 from keras.models import load_model
 import numpy as np
 from sklearn.metrics import accuracy_score
+from SharedClasses import CustomImageDataGenerator, f1_score
 
-model1_name='model1.h5'
-model2_name='model2.h5'
-model3_name='model3.h5'
+model1_name='models/belron-simple-256.keras'
+model2_name='models/repair-replace-resnet-cross.keras'
+model3_name='models/repair-replace-cross-256.keras'
+model4_name='models/belron-mobilenet.keras'
 
 
 def load_ensemble_models(model_filenames):
     models = []
     for filename in model_filenames:
-        models.append(load_model(filename))
+        models.append(load_model(filename, custom_objects={'f1_score': f1_score}))
     return models
 
 
-def ensemble_predictions(models, generator):
-    # Initialize an empty list to store predictions
-    ensemble_preds = []
 
-    # Iterate over the generator
+def ensemble_predictions(models, generator, total_images):
+    processed_images = 0
+
     for X_batch, _ in generator:
-        # Get predictions from each model for the current batch and average them
         batch_preds = [model.predict(X_batch) for model in models]
-        avg_batch_pred = np.mean(batch_preds, axis=0)
+        avg_batch_pred = np.mean(batch_preds, axis=0)  # Average predictions from all models
 
-        # Store the average predictions
-        ensemble_preds.append(avg_batch_pred)
+        # Process each prediction in the batch
+        for i, pred in enumerate(avg_batch_pred):
+            # pred contains the prediction values (probabilities) for each class
+            predicted_class = np.argmax(pred)
+            print(f'Processed image {processed_images + 1}/{total_images}: Predicted class = {predicted_class}, Prediction values = {pred}')
+            processed_images += 1
 
-    # Concatenate all batch predictions
-    final_predictions = np.concatenate(ensemble_preds, axis=0)
-    return final_predictions
+            if processed_images >= total_images:
+                break
+
+        if processed_images >= total_images:
+            break
 
 
-# Load models
-model_filenames = [model1_name, model2_name, model3_name]
+
+model_filenames = [model1_name, model2_name, model3_name, model4_name]
 models = load_ensemble_models(model_filenames)
 
-# Create an instance of your data generator
-image_directory = 'path/to/your/image/directory'
-image_width = 300  # Example width, adjust as needed
-image_height = 300 # Example height, adjust as needed
-batch_size = 8   # Example batch size, adjust as needed
+image_directory = 'images-testing/close_up'
+image_width = 256 
+image_height = 256
+batch_size = 4   
 
 data_generator = CustomImageDataGenerator(image_directory, image_width, image_height, batch_size)
 generator = data_generator.generate_data(is_training=False)
 
-# Generate ensemble predictions
-ensemble_pred = ensemble_predictions(models, generator)
+ensemble_pred = ensemble_predictions(models, generator, data_generator.calculate_num_samples())
 
 ensemble_labels = np.argmax(ensemble_pred, axis=1)
-y_test_labels = np.argmax(y_test, axis=1)  # Adjust if your y_test is not one-hot encoded
+y_test_labels = np.argmax(['repair','replace'], axis=1) 
 
-# Evaluate accuracy
 ensemble_accuracy = accuracy_score(y_test_labels, ensemble_labels)
 print(f'Ensemble Model Accuracy: {ensemble_accuracy}')
 
